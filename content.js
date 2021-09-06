01062ea3dc151b6a6c16ac6f744e53c4b6d114cc
KeithMcKiernan <keithmckiernan01@gmail.com>
2021-09-06 21:59:53 +0100

    chore: more cleanup

--- content.js
+++ content.js
@@ -1,60 +1,61 @@
-let data = [];
-let previousUrl = "";
-let allAnchorTags = [];
+{
+  let data = [];
+  let previousUrl = "";
+  let allAnchorTags = [];
 
-// @todo - move these styles to external stylesheet
-const styles = {
-  label: "color: red; font-weight: 600; font-size: 12px",
-  rank: 'font-family: "Poppins", sans-serif; position: absolute; bottom: 20px; left: 12px; color: #000; font-weight: 500; font-size: 11px',
-  rarity:
-    'font-family: "Poppins", sans-serif; position: absolute; bottom: 6px; left: 12px; color: #000; font-weight: 500; font-size: 11px',
-};
+  // @todo - move these styles to external stylesheet
+  const styles = {
+    label: "color: red; font-weight: 600; font-size: 12px",
+    rank: 'display: block; font-family: "Poppins", sans-serif; position: absolute; left: 12px; bottom: 20px; color: #000; font-weight: 500; font-size: 11px',
+    rarity:
+      'display: block; font-family: "Poppins", sans-serif; position: absolute; left: 12px; bottom: 6px; color: #000; font-weight: 500; font-size: 11px',
+  };
 
-// observe any changes to the url and
-// add/remove event listeners when required
-const observer = new MutationObserver(() => {
-  if (location.href !== previousUrl) {
-    previousUrl = location.href;
-    if (previousUrl.includes("/collection/g-evols")) {
-      document.addEventListener("scroll", onScroll, false);
-    } else {
-      document.removeEventListener("scroll", onScroll, false);
-    }
-  }
-});
-
-const onScroll = () => {
-  const tags = document.querySelectorAll(".Asset--anchor");
-  [].forEach.call(allAnchorTags, (a) => {
-    if (!allAnchorTags.includes(a.href)) {
-      allAnchorTags.push(a);
+  // observe any changes to the url and
+  // add/remove event listeners when required
+  const observer = new MutationObserver(() => {
+    if (location.href !== previousUrl) {
+      previousUrl = location.href;
+      if (previousUrl.includes("/collection/g-evols")) {
+        document.addEventListener("scroll", onScroll, false);
+      } else {
+        document.removeEventListener("scroll", onScroll, false);
+      }
     }
   });
 
-  for (const tag of tags) {
-    const idx = data.findIndex(({ id }) => id === +tag.href.split("/").pop());
-    if (idx !== -1) {
-      if (!document.querySelector(`#g-evol-rank-${idx}`)) {
-        const rank = document.createElement("div");
-        rank.id = `g-evol-rank-${idx}`;
-        rank.innerHTML = `<span style="${styles.label}">Rank:</span> #${data[idx].rank}`;
-        rank.style.cssText = styles.rank;
+  const onScroll = () => {
+    const tags = document.querySelectorAll(".Asset--anchor");
+    [].forEach.call(allAnchorTags, (a) => {
+      if (!allAnchorTags.includes(a.href)) {
+        allAnchorTags.push(a);
+      }
+    });
 
-        const rarity = document.createElement("div");
-        rarity.id = `g-evol-rarity-${idx}`;
-        rarity.innerHTML = `<span style="${styles.label}">Rarity Score:</span> ${data[idx].rarity}`;
-        rarity.style.cssText = styles.rarity;
+    for (const tag of tags) {
+      const idx = data.findIndex(({ id }) => id === +tag.href.split("/").pop());
+      if (idx !== -1) {
+        if (!document.querySelector(`#g-evol-rank-${idx}`)) {
+          const rank = document.createElement("div");
+          rank.id = `g-evol-rank-${idx}`;
+          rank.innerHTML = `<span style="${styles.label}">Rank:</span> #${data[idx].rank}`;
+          rank.style.cssText = styles.rank;
+          tag.appendChild(rank);
 
-        tag.appendChild(rank);
-        tag.appendChild(rarity);
+          const rarity = document.createElement("div");
+          rarity.id = `g-evol-rarity-${idx}`;
+          rarity.innerHTML = `<span style="${styles.label}">Rarity Score:</span> ${data[idx].rarity}`;
+          rarity.style.cssText = styles.rarity;
+          tag.appendChild(rarity);
+        }
       }
     }
-  }
-};
+  };
 
-observer.observe(document.body, { childList: true });
+  observer.observe(document.body, { childList: true });
 
-fetch("https://opensea-rarity-helper.vercel.app/g-evols/stats")
-  .then((response) => response.json())
-  .then((response) => (data = response))
-  .catch((error) => console.error(error));
+  fetch("https://opensea-rarity-helper.vercel.app/g-evols/stats")
+    .then((response) => response.json())
+    .then((response) => (data = response))
+    .catch((error) => console.error(error));
+}
--- popup.js
+++ popup.js
@@ -1,3 +1,16 @@
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
   chrome.tabs.executeScript(tabs[0].id, { file: "content.js" });
 });
+
+document.querySelector("#clipboard").addEventListener("click", () => {
+  navigator.clipboard
+    .writeText("0xEB9275bB2E859562E8314805CE0B39eAb8D0e313")
+    .then(() => {
+      document.querySelector("#clipboard").setAttribute("aria-label", "Copied");
+      setTimeout(() => {
+        document
+          .querySelector("#clipboard")
+          .setAttribute("aria-label", "Copy to clipboard");
+      }, 2500);
+    });
+});
